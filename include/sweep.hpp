017473860752836446a950fe2bf9cccaabba6ec7
alisher ashyrov <alisher_ashyrov@mail.ru>
2021-02-13 19:38:55 +0300

    feat: append sweep method impl

--- include/sweep.hpp
+++ include/sweep.hpp
@@ -5,4 +5,11 @@
 #ifndef SPDE_SERVER_INCLUDE_SWEEP_HPP_
 #define SPDE_SERVER_INCLUDE_SWEEP_HPP_
 
+#include <vector>
+
+namespace sweep {
+std::vector<float> solve(const std::vector<float> &a, const std::vector<float> &b,
+                         const std::vector<float> &c, const std::vector<float> &d);
+}
+
 #endif //SPDE_SERVER_INCLUDE_SWEEP_HPP_
--- src/sweep.cpp
+++ src/sweep.cpp
@@ -1,4 +1,42 @@
 //
 // Created by alisher on 13.02.2021.
 //
+#include <map>
+#include <iostream>
+#include <sweep.hpp>
+namespace sweep {
 
+std::vector<float> solve(const std::vector<float> &a, const std::vector<float> &b,
+                         const std::vector<float> &c, const std::vector<float> &d) {
+  if (a.empty() or b.empty() or c.empty() or d.empty()) {
+    throw std::runtime_error("[sweep.solve]: vectors are shouldn't be empty");
+  }
+  if (a.size() != b.size() or a.size() != c.size() or a.size() != d.size()) {
+    throw std::runtime_error("[sweep.solve]: must be a.size==b.size==c.size==d.size");
+  }
+  std::clog << "[sweep.solve]: start" << std::endl;
+
+  std::map<size_t, float> alpha, beta, gamma;
+
+  gamma[0] = b[0];
+  alpha[0] = -c[0] / gamma[0];
+  beta[0] = d[0] / gamma[0];
+  const auto m = d.size() - 1;
+  for (size_t i = 1; i < m; ++i) {
+    gamma[i] = b[i] + a[i] * alpha[i - 1];
+    alpha[i] = -c[i] / gamma[i];
+    beta[i] = (d[i] - a[i] * beta[i - 1]) / gamma[i];
+  }
+  gamma[m] = b[m] + a[m] * alpha[m - 1];
+  beta[m] = (d[m] - a[m] * beta[m - 1]) / gamma[m];
+
+  std::vector<float> x(d.size(), 0.0);
+  x[m] = beta[m];
+  for (size_t i = m - 1; i >= 0; --i) {
+    x[i] = alpha[i] * x[i + 1] + beta[i];
+  }
+  std::clog << "[sweep.solve]: done" << std::endl;
+  return x;
+}
+
+}
